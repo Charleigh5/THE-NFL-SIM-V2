d26342f8a6858bdbf16c99d235cf55217d079a55
Chris Weir <cweir45@gmail.com>
2025-12-20 01:53:15 -0500

    feat: add 3D trophy room page with interactive trophy case scene

--- frontend/src/components/trophy/TrophyCaseScene.tsx
+++ frontend/src/components/trophy/TrophyCaseScene.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment, ContactShadows, Stars } from "@react-three/drei";
 import { LombardiTrophy, MvpTrophy, DivisionTitleTrophy } from "./TrophyAssets";
-import { useTheme } from "../../context/ThemeContext";
+import { useTheme } from "../../context/useTheme";
 
 export const TrophyCaseScene = () => {
   const { activeTeam } = useTheme();
